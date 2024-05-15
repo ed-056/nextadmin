3230f56c2a48b4cc601b3253dbffa271eee50fe7
ed-056 <169015474+ed-056@users.noreply.github.com>
2024-05-15 21:13:02 +0545

    5/15/2024
    added few of the elements in dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,27 +1,16 @@
-import { cards } from "../lib/data";
 import Card from "../ui/dashboard/card/card";
-import Chart from "../ui/dashboard/chart/chart";
-import styles from "../ui/dashboard/dashboard.module.css";
-import Rightbar from "../ui/dashboard/rightbar/rightbar";
-import Transactions from "../ui/dashboard/transactions/transactions";
+import styles from "./page.module.css"
 
-const Dashboard = () => {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.main}>
-        <div className={styles.cards}>
-          {cards.map((item) => (
-            <Card item={item} key={item.id} />
-          ))}
-        </div>
-        <Transactions />
-        <Chart />
-      </div>
-      <div className={styles.side}>
-        <Rightbar />
-      </div>
-    </div>
-  );
-};
 
-export default Dashboard;
+
+const Dashboard = ({}) => {
+  return <div className={styles.container}>
+    <Card></Card>
+    
+     </div>
+}
+
+export default Dashboard        
+
+
+
---
+++ app/frontend/product/product.jsx
@@ -0,0 +1,53 @@
+import styles from './product.module.css'
+import Image from 'next/image';
+const item = [
+    {
+      title:"pages",
+      list:[
+        {
+          title:"Dashboard",
+          path:"/dashboard",
+          
+        },
+        {
+          title:"Users",
+          path:"/dashboard/users",
+          
+        },
+        {
+          title:"Products",
+          path:"/dashboard/products",
+         
+        },
+        {
+          title:"Transactions",
+          path:"/dashboard/transactions",
+          
+        }
+      ]
+    },
+
+];
+const Product = ({}) => {
+
+
+  return <div className={styles.container}>
+    <div className={styles.content}>
+        <div className={styles.card}>
+          <div className={styles.img}>
+            <img className={styles.image} src="/avatar.png" layout=""></img>
+          </div>
+          <div className={styles.desc}>
+            <div className={styles.title}>WITCH SPELL</div>
+            <div className={styles.price}>From ~ $80</div>
+
+          </div>
+          
+        </div>
+    </div>
+
+
+  </div>
+}
+
+export default Product

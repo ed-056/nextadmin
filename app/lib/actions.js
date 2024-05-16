46bd26d53f5ed6110a5ca5b7f45376ea87248e2b
ed-056 <169015474+ed-056@users.noreply.github.com>
2024-05-16 11:03:00 +0545

    Revert "Login redirecting problem fixed!"
    This reverts commit 51e70353c147fbe1ff40bb1193ea19b5c9f108e3.

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -160,9 +160,6 @@ export const authenticate = async (prevState, formData) => {
   try {
     await signIn("credentials", { username, password });
   } catch (err) {
-    if (err.message.includes("CredentialsSignin")) {
-      return "Wrong Credentials";
-    }
-    throw err;
+    return "Wrong Credentials!";
   }
 };
